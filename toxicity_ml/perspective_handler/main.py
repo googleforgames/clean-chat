cd2eec64aff4a1ae1484bbba0acac3a7f457eaab
Patrick Smith <64227881+smithpatrick12@users.noreply.github.com>
2020-09-24 17:09:47 -0400

    Create main.py

---
+++ toxicity_ml/perspective_handler/main.py
@@ -0,0 +1,32 @@
+from toxic_tests import Perspective_Handler, monitor_toxicity
+from itertools import count
+
+key = 'API_KEY'
+
+c = 0
+pList = []
+
+# Python Input for Comment Testing
+while True:
+  text = input()
+  if isinstance(text, binary_type):
+    text = text.decode('utf-8')
+
+  toxicP = Perspective_Handler(key)
+  monitorT = monitor_toxicity()
+
+  # Returns the analyzed comment from the Perspective API
+  comment = toxicP.analyze_comment(text)
+  # Parse through the Json Dictionaries 
+  probs = toxicP.parse_json(comment)
+ 
+  # Calculte Total Prob
+  p = monitorT.calculate_tScore(probs)
+
+  # For continuous talk steams
+  if p > .85:
+    pList.append(p)
+    c = c+1
+    if c > 1:
+      newP = monitorT.calculate_score(c, pList[(c-1)], p)
+      print ("Please Remember to Be Kind")
