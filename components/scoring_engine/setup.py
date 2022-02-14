7f8506d73c5bf50e0aba532e8e0703deabc71440
Dan Zaratsian <dtz001@gmail.com>
2022-02-14 14:03:32 +0000

    Rename to clean-chat and updated config

--- components/scoring_engine/setup.py
+++ components/scoring_engine/setup.py
@@ -23,6 +23,6 @@ setup(name='scoring_logic',
       description='Realtime scoring logic package for Dataflow',
       author='GoogleforGames',
       author_email='googleforgames@google.com',
-      url='https://github.com/googleforgames/antidote',
+      url='https://github.com/googleforgames/clean-chat',
       license='Apache',
       packages=['scoring_logic'])
--- examples/api-backend-post-audio.py
+++ examples/api-backend-post-audio.py
@@ -126,7 +126,7 @@ def main():
     
     # Demo/Example JSON Payload for Text Chat
     # Alternative URL for audio testing: https://github.com/zaratsian/Datasets/raw/master/test_audio.mp3
-    # Alternative URL for audio testing: https://github.com/googleforgames/antidote/raw/master/assets/test_audio.mp3
+    # Alternative URL for audio testing: https://github.com/googleforgames/clean-chat/raw/master/assets/test_audio.mp3
     post_json ={
         'audio_uri': 'https://github.com/zaratsian/Datasets/raw/master/test_audio.mp3',
         'userid':    'user123'
